15d9b42c3fcd3f23f174aa5f01cb32bb3545d948
Mat Miehle <mat@miehle.org>
2019-03-29 10:42:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
 #test
+terraform {
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "terraform-state-20190319192448961100000002"
+    key            = "root/terraform.tfstate"
+    dynamodb_table = "terraform_state_lock"
+  }
+}
